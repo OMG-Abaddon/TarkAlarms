a3b7db7b16f6ad8a3647d52c31b68f28d5822f21
Héctor Álvarez <zeratul1992@hotmail.com>
2021-09-17 21:54:43 +0200

    Added a few changes and made a viable example. Missing alarm debug stuff

--- TarkAlarms/Classes/Trader.cs
+++ TarkAlarms/Classes/Trader.cs
@@ -1,58 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
 using System.Text;
 using System.Timers;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace TarkAlarms.Classes
 {
-    public enum TraderNames
+    public static class Extension
     {
-        Prapor,
-        Therapist,
-        Fence,
-        Skier,
-        Peacekeeper,
-        Mechanic,
-        Ragman,
-        Jaeger,
+        // https://stackoverflow.com/questions/1110789/get-list-element-position-in-c-sharp-using-linq
+        public static IEnumerable<int> FindEveryIndex<T>(this IEnumerable<T> items,
+                                                         Predicate<T> predicate)
+        {
+            int index = 0; bool found = false;
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    found = true; yield return index;
+                };
+                index++;
+            }
+            if (!found) yield return -1;
+        }
 
+        public static int FirstIndexOf<T>(this IEnumerable<T> collection, string value)
+        {
+            int index = 0;
+            foreach (T item in collection)
+            {
+                if (item.ToString() == value)
+                {
+                    return index;
+                };
+                index++;
+            }
+            return -1;
+        }
     }
+
 
     public class Trader
     {
+        private static string defaultSoundFile = "default";
+        private string pathWavAlert;
+
+        public string Name { get; set; }
+        public DispatcherTimer RestockTimer { get; set; }
 
 
-        public string Name { get; private set; }
-        public DateTime LastRestock { get; set; }
-        public DateTime NextRestock { get; set; }
-        public TimeSpan RestockTime { get; set; }
-
-        public Trader(string name, DateTime nextRestock, DateTime lastRestock, TimeSpan restockTime)
+        public Trader(string name, TimeSpan restockTime)
         {
             Name = name;
-            LastRestock = lastRestock;
-            NextRestock = nextRestock;
-            RestockTime = restockTime;
+            RestockTimer = new DispatcherTimer();
+            RestockTimer.Interval = restockTime;
+
+            pathWavAlert = Path.Combine(System.AppDomain.CurrentDomain.BaseDirectory, $@"sounds\default.wav");
         }
 
-        public Trader(string name) : this(name, DateTime.MaxValue, DateTime.Now, TimeSpan.MaxValue)
+        internal void SetTimerControl(TextBlock textBlock)
+        {
+            RestockTimer.Tick += new EventHandler((s, e) => PlaySound());
+        }
+
+        public Trader(string name) :
+            this(name, new TimeSpan(24, 0, 0))
         {
 
         }
 
-        public Trader(TraderNames name) : this(name.ToString())
+        public void StartRestockTimer()
         {
-
+            RestockTimer.Start();
         }
 
-        //public Trader(string name, DateTime nextRestock, DateTime lastRestock) 
-        //    : this(name, nextRestock, DateTime.MaxValue, TimeSpan.Zero) { }
-
-        //public Trader(string name, DateTime nextRestock) 
-        //    : this(name, nextRestock, DateTime.MaxValue) { }
-
-        //public Trader(string name) 
-        //    : this(name, DateTime.MaxValue) { }
-
+        private void PlaySound()
+        {
+            using (SoundPlayer soundPlayer = new SoundPlayer())
+            {
+                soundPlayer.SoundLocation = pathWavAlert;
+                soundPlayer.Load();
+                soundPlayer.Play();
+            }
+        }
     }
 }
--- TarkAlarms/MainWindow.xaml.cs
+++ TarkAlarms/MainWindow.xaml.cs
@@ -3,6 +3,10 @@ using System.Timers;
 using System;
 using System.Collections.Generic;
 using TarkAlarms.Classes;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Linq;
 
 namespace TarkAlarms
 {
@@ -11,37 +15,116 @@ namespace TarkAlarms
     /// </summary>
     public partial class MainWindow : Window
     {
-        public Timer RestockTimer { get; set; }
-        private Trader[] _Traders;
+        private DispatcherTimer restockTimer;
 
-        public Trader[] Traders
+        public List<Trader> Traders { get; private set; }
+
+        public List<string> TraderData = new List<string>()
         {
-            get { return _Traders; }
-            set { _Traders = value; }
-        }
+            "Prapor",
+            "Therapist",
+            "Fence",
+            "Skier",
+            "Peacekeeper",
+            "Mechanic",
+            "Ragman",
+            "Jaeger",
+        };
+
+        //public List<string> TraderDictionary = new Dictionary<int, string>()
+        //    {
+        //        {0,"Prapor"},
+        //        {1,"Therapist"},
+        //        {2,"Fence"},
+        //        {3,"Skier"},
+        //        {4,"Peacekeeper"},
+        //        {5,"Mechanic"},
+        //        {6,"Ragman"},
+        //        {7,"Jaeger"},
+
+        //    };
 
         public MainWindow()
         {
             InitializeComponent();
+            InitializeApp();
         }
 
         private void InitializeApp()
         {
-            _Traders = new Trader[]
-            {
-                new Trader(TraderNames.Prapor),
-                new Trader(TraderNames.Therapist),
-                new Trader(TraderNames.Fence),
-                new Trader(TraderNames.Skier),
-                new Trader(TraderNames.Peacekeeper),
-                new Trader(TraderNames.Mechanic),
-                new Trader(TraderNames.Ragman),
-                new Trader(TraderNames.Jaeger),
-            };
+            Traders = new List<Trader>();
+            Trader prapor = new Trader("Prapor"),
+                therapist = new Trader("Therapist"),
+                fence = new Trader("Fence"),
+                skier = new Trader("Skier"),
+                peacekeeper = new Trader("Peacekeeper"),
+                mechanic = new Trader("Mechanic"),
+                ragman = new Trader("Ragman"),
+                jaeger = new Trader("Jaeger");
 
-            RestockTimer.Start();
-            //RestockTimer.
+            prapor.SetTimerControl(txbClockPrapor);
+            prapor.RestockTimer.Interval = new TimeSpan(0, 0, 2);
+            prapor.StartRestockTimer();
+
+            //foreach (Trader trader in Traders)
+            //{
+            //    switch (trader.Name)
+            //    {
+            //        case TraderNames.Prapor:
+            //            break;
+            //        default:
+            //            break;
+            //    }
+            ////}
+
+            //Traders.Add(new Trader(TraderNames.Prapor));
+            //Traders.Add(new Trader(TraderNames.Therapist));
+            //Traders.Add(new Trader(TraderNames.Fence));
+            //Traders.Add(new Trader(TraderNames.Skier));
+            //Traders.Add(new Trader(TraderNames.Peacekeeper));
+            //Traders.Add(new Trader(TraderNames.Mechanic));
+            //Traders.Add(new Trader(TraderNames.Ragman));
+            //Traders.Add(new Trader(TraderNames.Jaeger));
+
+
+
+
+
+
+
+
+            //{
+            //    new Trader(, DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Therapist", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Fence", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Skier", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Peacekeeper", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Mechanic", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Ragman", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //    new Trader("Jaeger", DateTime.Now.AddMinutes(5),DateTime.Now.AddMinutes(-20),new TimeSpan(0,25,0)),
+            //};
+
+
+
+            //this.Traders = new Trader[]
+            //{
+            //    new Trader(TraderNames.Prapor),
+            //    new Trader(TraderNames.Therapist),
+            //    new Trader(TraderNames.Fence),
+            //    new Trader(TraderNames.Skier),
+            //    new Trader(TraderNames.Peacekeeper),
+            //    new Trader(TraderNames.Mechanic),
+            //    new Trader(TraderNames.Ragman),
+            //    new Trader(TraderNames.Jaeger),
+            //};
+
+            //foreach (Trader trader in this.Traders)
+            //{
+            //    trader.RestockTimer.Tick += (s, e) => MainWindow.
+            //    trader.StartRestockTimer();
+            //}
         }
+
     }
 
 
