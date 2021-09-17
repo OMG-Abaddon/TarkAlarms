b2d6dc5fc2d2d952be7425d1d005af2cdc0d9c7f
Abaddon <Abaddon@ABADDON-PC>
2021-09-17 16:14:24 +0200

    Add project files.

---
+++ TarkAlarms/Classes/Trader.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace TarkAlarms.Classes
+{
+    public enum TraderNames
+    {
+        Prapor,
+        Therapist,
+        Fence,
+        Skier,
+        Peacekeeper,
+        Mechanic,
+        Ragman,
+        Jaeger,
+
+    }
+
+    public class Trader
+    {
+
+
+        public string Name { get; private set; }
+        public DateTime LastRestock { get; set; }
+        public DateTime NextRestock { get; set; }
+        public TimeSpan RestockTime { get; set; }
+
+        public Trader(string name, DateTime nextRestock, DateTime lastRestock, TimeSpan restockTime)
+        {
+            Name = name;
+            LastRestock = lastRestock;
+            NextRestock = nextRestock;
+            RestockTime = restockTime;
+        }
+
+        public Trader(string name) : this(name, DateTime.MaxValue, DateTime.Now, TimeSpan.MaxValue)
+        {
+
+        }
+
+        public Trader(TraderNames name) : this(name.ToString())
+        {
+
+        }
+
+        //public Trader(string name, DateTime nextRestock, DateTime lastRestock) 
+        //    : this(name, nextRestock, DateTime.MaxValue, TimeSpan.Zero) { }
+
+        //public Trader(string name, DateTime nextRestock) 
+        //    : this(name, nextRestock, DateTime.MaxValue) { }
+
+        //public Trader(string name) 
+        //    : this(name, DateTime.MaxValue) { }
+
+    }
+}
---
+++ TarkAlarms/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Timers;
+using System;
+using System.Collections.Generic;
+using TarkAlarms.Classes;
+
+namespace TarkAlarms
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public Timer RestockTimer { get; set; }
+        private Trader[] _Traders;
+
+        public Trader[] Traders
+        {
+            get { return _Traders; }
+            set { _Traders = value; }
+        }
+
+        // Using a DependencyProperty as the backing store for Traders.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TradersProperty =
+            DependencyProperty.Register("Traders", typeof(Trader[]), typeof(ownerclass), new PropertyMetadata(0));
+
+
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeApp()
+        {
+            _Traders = new Trader[]
+            {
+                new Trader(TraderNames.Prapor),
+                new Trader(TraderNames.Therapist),
+                new Trader(TraderNames.Fence),
+                new Trader(TraderNames.Skier),
+                new Trader(TraderNames.Peacekeeper),
+                new Trader(TraderNames.Mechanic),
+                new Trader(TraderNames.Ragman),
+                new Trader(TraderNames.Jaeger),
+            };
+
+            RestockTimer.Start();
+            //RestockTimer.
+        }
+    }
+
+
+}
