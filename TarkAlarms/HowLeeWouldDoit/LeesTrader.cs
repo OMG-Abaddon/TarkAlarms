f453a51f96ba11790a4a301b180110de08f8717a
Lee Butler <butler1233@hotmail.com>
2021-09-17 23:25:33 +0100

    Oh right yeah

---
+++ TarkAlarms/HowLeeWouldDoit/LeesTrader.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Threading;
+
+namespace TarkAlarms.HowLeeWouldDoit
+{
+    public class LeesTrader : FuckDependencyProperties
+    {
+        /// <summary>
+        /// The name fo the trader. Ultimately irrelevant to the timer  other than looking pretty
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// When the timer last reset.
+        /// </summary>
+        public DateTime ResetTime { get; set; }
+
+        /// <summary>
+        /// How long the timer lasts
+        /// </summary>
+        public TimeSpan RestockTime { get; set; }
+
+        /// <summary>
+        /// If the timer will auto reset when it runs out
+        /// </summary>
+        public bool AutoReset { get; set; }
+
+        /// <summary>
+        /// If the timer will play noise when it resets
+        /// </summary>
+        public bool AudibleAlarm { get; set; }
+
+        public double PercentageComplete
+        {
+            get => 1 - ((DateTime.UtcNow - ResetTime) / RestockTime);
+        }
+
+        private DispatcherTimer _internalTimer;
+
+        private void TimerTick(object? state)
+        {
+            if (DateTime.UtcNow > (ResetTime + RestockTime))
+            {
+                _internalTimer.Stop();
+                if (AutoReset) ResetTimer();
+                if (AudibleAlarm) SoundPlayer.PlayDefault();
+            }
+        }
+
+        LeesTrader()
+        {
+            
+            _internalTimer = new DispatcherTimer(DispatcherPriority.Normal);
+            _internalTimer.Interval = TimeSpan.FromSeconds(1);
+            _internalTimer.Start();
+
+        }
+
+        public void ResetTimer(DateTime? resetTime = null)
+        {
+            _internalTimer.Stop();
+            var actualResetTime = resetTime ?? DateTime.UtcNow;
+            ResetTime = actualResetTime;
+            _internalTimer.Start();
+        }
+    }
+}
---
+++ TarkAlarms/HowLeeWouldDoit/SoundPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Accessibility;
+
+namespace TarkAlarms.HowLeeWouldDoit
+{
+    static class SoundPlayer
+    {
+        private const string DEFAULT_FILENAME = "Sounds/default.wav";
+
+        private static bool _initiated;
+        private static System.Media.SoundPlayer _player;
+
+        /// <summary>
+        /// Plays the wav async. No idea what happens if you try to play multiple at the same time. Maybe it'd be wise to not be static, but wavs are heavy, so this is better
+        /// </summary>
+        internal static void PlayDefault()
+        {
+            if (!_initiated) InitPlayer();
+            _player.Play();
+        }
+
+        private static void InitPlayer()
+        {
+            _player = new System.Media.SoundPlayer(DEFAULT_FILENAME);
+            _player.Load(); //fuck async lol I'm lazy
+
+        }
+
+    }
+}
