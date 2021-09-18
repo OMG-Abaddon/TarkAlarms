2d5f3a2013b3af4741838451520a2c658419656b
OMG-Abaddon <zeratul1992@hotmail.com>
2021-09-18 13:17:26 +0200

    Refactored a few things and moved timerwindow to mainwindow.

--- TarkAlarms/MainWindow.xaml.cs
+++ TarkAlarms/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
 using TarkAlarms.Traders;
 
 namespace TarkAlarms
@@ -10,60 +13,35 @@ namespace TarkAlarms
     {
 
         //public List<Trader> Traders { get; private set; }
+        public ObservableCollection<Trader> Traders { get; set; } = new ObservableCollection<Trader>();
 
-        //public List<string> TraderData = new List<string>()
-        //{
-        //    "Prapor",
-        //    "Therapist",
-        //    "Fence",
-        //    "Skier",
-        //    "Peacekeeper",
-        //    "Mechanic",
-        //    "Ragman",
-        //    "Jaeger",
-        //};
+        public List<string> AvailableTraders = new List<string>()
+        {
+            "Prapor",
+            "Therapist",
+            "Fence",
+            "Skier",
+            "Peacekeeper",
+            "Mechanic",
+            "Ragman",
+            "Jaeger",
+        };
 
 
         public MainWindow()
         {
             InitializeComponent();
-            //InitializeApp();
+            AddTraders();
         }
 
-        //        private void InitializeApp()
-        //        {
-        //            Traders = new List<Trader>();
-        //            Trader prapor = new Trader("Prapor"),
-        //                therapist = new Trader("Therapist"),
-        //                fence = new Trader("Fence"),
-        //                skier = new Trader("Skier"),
-        //                peacekeeper = new Trader("Peacekeeper"),
-        //                mechanic = new Trader("Mechanic"),
-        //                ragman = new Trader("Ragman"),
-        //                jaeger = new Trader("Jaeger");
-        //#if DEBUG
-        //            prapor.RestockTimer.Interval = new TimeSpan(0, 0, 5); //test
-        //#endif
-        //            prapor.SetTimerControl(txbClockPrapor);
-        //            therapist.SetTimerControl(txbClockTherapist);
-        //            fence.SetTimerControl(txbClockFence);
-        //            skier.SetTimerControl(txbClockSkier);
-        //            peacekeeper.SetTimerControl(txbClockPeacekeeper);
-        //            mechanic.SetTimerControl(txbClockMechanic);
-        //            ragman.SetTimerControl(txbClockRagman);
-        //            jaeger.SetTimerControl(txbClockJaeger);
+        private void AddTraders()
+        {
+            foreach (var traderName in AvailableTraders)
+            {
+                Traders.Add(new Trader { Name = traderName });
+            }
+        }
 
-        //            prapor.StartRestockTimer();
-        //            therapist.StartRestockTimer();
-        //            fence.StartRestockTimer();
-        //            skier.StartRestockTimer();
-        //            peacekeeper.StartRestockTimer();
-        //            mechanic.StartRestockTimer();
-        //            ragman.StartRestockTimer();
-        //            jaeger.StartRestockTimer();
-
-
-        //        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
